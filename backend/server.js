80914874bce654e3ad99b7ace0412d4cbbb1bdd5
Your Name <dicksonwachira886@gmail.com>
2026-01-23 13:13:33 +0300

    Set up server, routes, and DB connection

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import authRoutes from './routes/auth.route.js';
+import { ENV_VARS } from './config/envVars.js';
+import { connectDB } from './config/db.js';
+
+const app = express();
+const PORT = ENV_VARS.PORT || 5000;
+
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+app.use("/api/v1/auth/", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    connectDB();
+});
+
