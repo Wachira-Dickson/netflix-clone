942fdd62072146c52df393edd0a4d927ed315404
Your Name <dicksonwachira886@gmail.com>
2026-03-17 17:02:36 +0300

    Style the search functionality on the search page

---
+++ frontend/src/components/skeletons/WatchPageSkeleton.jsx
@@ -0,0 +1,14 @@
+const WatchPageSkeleton = () => {
+  return (
+    <div clasName='animate-pulse'>
+        <div clasName='bg-gray-700 rounded-md w-40 h-6 mb-4 shimmer'></div>
+        <div clasName='bg-gray-700 rounded-md w-full h-96 mb-4 shimmer'></div>
+        <div clasName='bg-gray-700 rounded-md w-3/4 h-6 mb-2 shimmer'></div>
+        <div clasName='bg-gray-700 rounded-md w-1/2 h-6 mb-4 shimmer'></div>
+        <div clasName='bg-gray-700 rounded-md w-full h-24 shimmer'></div>
+
+    </div>
+  )
+}
+
+export default WatchPageSkeleton
